22e7b2117b1612de0e18894b88b9a9c7357e277f
KatiBar <katarzyna.kinga.baranska@gmail.com>
2023-05-04 23:05:12 +0200

    Init commit

---
+++ src/main/java/com/example/jjdind49_katibar_zad27_1/JjdInd49KatiBarZad271Application.java
@@ -0,0 +1,13 @@
+package com.example.jjdind49_katibar_zad27_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjdInd49KatiBarZad271Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjdInd49KatiBarZad271Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jjdind49_katibar_zad27_1/JjdInd49KatiBarZad271ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jjdind49_katibar_zad27_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjdInd49KatiBarZad271ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
